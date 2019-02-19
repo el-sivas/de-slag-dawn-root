4fdaa8827638a75cb0ecee8edd3a2c7640c2b61f
sivas <sivas@enceladus>
2019-02-19 07:50:57 +0100

    root

---
+++ dawn-root/dawn-root-base/src/main/java/de/slag/root/base/SlagConstants.java
@@ -0,0 +1,9 @@
+package de.slag.root.base;
+
+import java.time.LocalDateTime;
+
+public class SlagConstants {
+
+	public static final LocalDateTime END_OF_DEKAMILLENIAL = LocalDateTime.of(9999, 31, 12, 23, 59, 59);
+
+}
