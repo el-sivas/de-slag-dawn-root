eff20924f1217bc5ab46bad5cf2eb678613f8749
sebastian <sebastian@kallisto.fritz.box>
2019-02-20 12:05:30 +0100

    model bean et al

---
+++ dawn-root/dawn-root-base/src/main/java/de/slag/root/base/Labelable.java
@@ -0,0 +1,11 @@
+package de.slag.root.base;
+
+
+public interface Labelable {
+	
+	/**
+	 * @return A human readable label of this.
+	 */
+	String getLabel();
+
+}
---
+++ dawn-root/dawn-root-base/src/main/java/de/slag/root/base/ModelBean.java
@@ -0,0 +1,15 @@
+package de.slag.root.base;
+
+public abstract class ModelBean {
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
