b13198d465d1b23b4ea31fc60b878cf9314ac60f
sebastian <sebastian@kallisto.fritz.box>
2019-02-20 16:42:53 +0100

    model bean valid until

--- dawn-root/dawn-root-base/src/main/java/de/slag/root/base/ModelBean.java
+++ dawn-root/dawn-root-base/src/main/java/de/slag/root/base/ModelBean.java
@@ -1,8 +1,12 @@
 package de.slag.root.base;
 
+import java.util.Date;
+
 public abstract class ModelBean {
 
 	private Long id;
+	
+	private Date validUntil;
 
 	public Long getId() {
 		return id;
@@ -10,6 +14,14 @@ public abstract class ModelBean {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Date getValidUntil() {
+		return validUntil;
+	}
+
+	public void setValidUntil(Date validUntil) {
+		this.validUntil = validUntil;
 	}
 
 }
