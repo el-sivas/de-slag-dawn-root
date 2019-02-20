1500587990c54274feb347f58fa01c84aa5a80b4
sebastian <sebastian@kallisto.fritz.box>
2019-02-20 12:58:31 +0100

    db connection test

--- dawn-root/dawn-root-base/src/main/java/de/slag/root/base/SlagConfigSupport.java
+++ dawn-root/dawn-root-base/src/main/java/de/slag/root/base/SlagConfigSupport.java
@@ -1,46 +1,59 @@
 package de.slag.root.base;
 
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Properties;
+
 public final class SlagConfigSupport {
 
 	public static <T extends DatabaseConfig> T getDefault(final Class<T> configType) {
-		
+
 		if (configType == DatabaseConfig.class) {
 			return configType.cast(defaultDatabase());
 		}
 		throw new BaseException("Not supported: " + configType);
-
 	}
 
 	private static DatabaseConfig defaultDatabase() {
+		final String defaultDbConfigProperty = "DefaultDatabaseConfig";
+		final Optional<String> defaultDatabaseConfig = SystemUtils.getSystemProperty(defaultDbConfigProperty);
+		if (!defaultDatabaseConfig.isPresent()) {
+			throw new BaseException("Property not set: '" + defaultDbConfigProperty + "'");
+		}
+		final Properties properties = PropertyUtils.fromFile(defaultDatabaseConfig.get());
+
+		final String username = (String) Objects.requireNonNull(properties.get("username"), "'username' not found");
+		final String url = (String) Objects.requireNonNull(properties.get("url"), "'url' not found");
+		final String password = (String) Objects.requireNonNull(properties.get("password"), "'password' not found");
+		final String driverClass = (String) Objects.requireNonNull(properties.get("driverClass"),
+				"'driverClass' not found");
+		final String dialect = (String) Objects.requireNonNull(properties.get("dialect"), "'dialect' not found");
+
 		return new DatabaseConfig() {
-			
+
 			@Override
 			public String getUsername() {
-				return "test";
+				return username;
 			}
-			
+
 			@Override
 			public String getUrl() {
-				// TODO Auto-generated method stub
-				return null;
+				return url;
 			}
-			
+
 			@Override
 			public String getPassword() {
-				// TODO Auto-generated method stub
-				return null;
+				return password;
 			}
-			
+
 			@Override
 			public String getDriverClass() {
-				// TODO Auto-generated method stub
-				return null;
+				return driverClass;
 			}
-			
+
 			@Override
 			public String getDialect() {
-				// TODO Auto-generated method stub
-				return null;
+				return dialect;
 			}
 		};
 	}
