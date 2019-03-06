46d11e336d8769cc007b5693c0c281f4aafc456f
sebastian <sebastian@kallisto.fritz.box>
2019-03-06 08:33:41 +0100

    correction constant

--- dawn-root/dawn-root-base/src/main/java/de/slag/root/base/SlagConstants.java
+++ dawn-root/dawn-root-base/src/main/java/de/slag/root/base/SlagConstants.java
@@ -4,6 +4,6 @@ import java.time.LocalDateTime;
 
 public class SlagConstants {
 
-	public static final LocalDateTime END_OF_DEKAMILLENIAL = LocalDateTime.of(9999, 31, 12, 23, 59, 59);
+	public static final LocalDateTime END_OF_DEKAMILLENIAL = LocalDateTime.of(9999, 12, 31, 23, 59, 59);
 
 }
